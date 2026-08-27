ba9df9f3ba5ab81625638ce9697e6a2c6776e417
dosantosdev <dosantosdev@gmail.com>
2026-08-27 14:40:03 -0300

    fix: center validation modal

--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -16,9 +16,10 @@ const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
   return createPortal(
     <dialog
       ref={dialog}
-      className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md"
+      className="fixed left-1/2 top-1/2 m-0 w-[90%] max-w-lg -translate-x-1/2 -translate-y-1/2 rounded-md p-4 shadow-md backdrop:bg-stone-900/90"
     >
       {children}
+
       <form method="dialog" className="mt-4 text-right">
         <Button>{buttonCaption}</Button>
       </form>
